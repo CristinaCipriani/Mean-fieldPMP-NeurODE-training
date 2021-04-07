9c5e0b9c6cc4b0a55f2629936d6a7b356de7908d
CristinaCipriani <81622069+CristinaCipriani@users.noreply.github.com>
2021-04-07 12:19:41 +0200

    updating

--- monodimensional.py
+++ monodimensional.py
@@ -13,18 +13,21 @@ from modules.analysis_1D import plot_loss_fct
 from modules.analysis_1D import loss_comparison_Lambda, loss_comparison_dt
 
 parser = argparse.ArgumentParser(description='Description of all the parameters below')
-parser.add_argument("--dt", default=0.1, help="This is time-discretization dt")
+parser.add_argument("--bias", default = False,
+                    help="This decides if the activation function contains a bias or not")
 parser.add_argument("--mu_0",
                     choices=["bigaussian", "gaussian"],
                     required=True, type=str,
                     help="This decides if the initial distirbution mu_0 is a bimodal or unimodal gaussian")
+parser.add_argument("--dt", default=0.1, help="This is time-discretization dt")
 parser.add_argument("--Lambda", default=0.1, help="This is regularization parameter lambda")
 parser.add_argument("--iterations", default=10, help="This is the number of outer iterations (of the shooting method)")
 
 args = parser.parse_args()
-dt = args.dt
 mu_0 = args.mu_0
-Lambda = args.Lambda
+bias = args.bias
+dt = args.dt
+lbd = args.Lambda
 num_iterations = args.iterations
 
 
@@ -53,11 +56,20 @@ else:
     y_left = -1
     y_right = 1
 
-#Initial guess of theta
-theta = np.ones((Nt-1,d))
-
-#Activation function
-def F(x, theta):
+#Activation functions
+def F_nobias(x, theta):
     return np.tanh(theta*x)
 
-print("END")
+def F_bias(x, theta):
+    return np.tanh(theta[:,0]*x + theta[:,1])
+
+if bias == False:
+    theta = np.ones((Nt-1,d))
+    F = F_nobias
+    Lambda = lbd
+else:
+    theta = np.ones((Nt-1,d,2))
+    F = F_bias
+    Lambda = [lbd,1]
+
+print("Prova a stampare lambda %s" %Lambda)
